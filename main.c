47781da3733b0540d655b5a2d76bc85887679b7f
keer2792 <keer.wang@bsu.edu>
2026-02-11 21:04:44 -0500

    implement vulnerable functions

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /* Project 0 - Input Validation and Buffer Overruns
  * Author: Keer Wang
- * Purpose: initial skeleton for the project
+ * Purpose: demonstrate vulnerable input handling and buffer overrun
+ *          memory can be accessed directly without validation.
  */
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <limits.h>
 #include <stdlib.h>
 
 #define MAX_USERS 10
@@ -14,11 +14,8 @@
 #define EXIT_VALUE 99
 
 void print_this_user_info(unsigned short userindex, char username[], unsigned short userpin, bool userIsAdmin);
-bool get_user_preference();
 int get_user_to_modify_vulnerable();
 void change_pin_vulnerable(int user_i, unsigned short u_pin[], int new_pin);
-int get_user_to_modify_more_secure(int current_num_users);
-bool change_pin_more_secure(int u_index, unsigned short u_pin[], int new_pin);
 
 int main(void) {
     struct {
@@ -27,11 +24,11 @@ int main(void) {
         char user_name[MAX_USERS][MAX_NAME_LEN];
     } user_data;
 
-    int i, num_users = 0;
-    int user_index = 0, new_pin = 0;
+    int i;
+    int num_users = 0;
+    int user_index = 0;
+    int new_pin = 0;
     char buffer[256] = "";
-    bool vulnerable_mode = false;
-    bool success = false;
 
     /* initialize memory */
     memset(user_data.user_isAdmin, 0, sizeof(user_data.user_isAdmin));
@@ -50,42 +47,56 @@ int main(void) {
 
     num_users = 2;
 
-    /* ask user preference */
-    vulnerable_mode = get_user_preference();
+    printf("Running VULNERABLE version (no input validation!)\n");
 
-    /* main loop placeholder */
+    /* main loop */
     while (1) {
+        /* print all users */
         for (i = 0; i < num_users; i++) {
-            print_this_user_info(i, user_data.user_name[i],
-                                 user_data.user_pin[i], user_data.user_isAdmin[i]);
+            print_this_user_info(i,
+                                 user_data.user_name[i],
+                                 user_data.user_pin[i],
+                                 user_data.user_isAdmin[i]);
         }
+
         printf("-------------\n");
 
-        if (vulnerable_mode) {
-            printf("VULNERABLE functionality will go here\n");
-        } else {
-            printf("SECURE functionality will go here\n");
-        }
+        /* vulnerable flow */
+        user_index = get_user_to_modify_vulnerable();
+
+        printf("Enter new PIN: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        sscanf(buffer, "%d", &new_pin);
+
+        change_pin_vulnerable(user_index, user_data.user_pin, new_pin);
     }
 
     return 0;
 }
 
-/* function skeletons */
+/* ---------------- helper functions ---------------- */
 
 void print_this_user_info(unsigned short userindex, char username[],
                           unsigned short userpin, bool userIsAdmin) {
     printf("---User Index #%d---\n", userindex);
-    printf("Name: %s\nPIN: %u\tisAdmin: %d\n", username, userpin, userIsAdmin);
+    printf("Name: %s\n", username);
+    printf("PIN: %u\tisAdmin: %d\n", userpin, userIsAdmin);
 }
 
+/* intentionally vulnerable */
+int get_user_to_modify_vulnerable(void) {
+    char buffer[256] = "";
+    int index = 0;
 
-bool get_user_preference() {
-    printf("Menu: 1) Vulnerable 2) Secure (default)\n");
-    return false;  /* default is secure */
+    printf("Enter user index to modify (%d to quit): ", EXIT_VALUE);
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "%d", &index);
+
+    if (index == EXIT_VALUE) exit(0);
+
+    return index;  /* NO validation */
 }
 
-int get_user_to_modify_vulnerable() { return -1; }
-void change_pin_vulnerable(int user_i, unsigned short u_pin[], int new_pin) {}
-int get_user_to_modify_more_secure(int current_num_users) { return -1; }
-bool change_pin_more_secure(int user_i, unsigned short u_pin[], int new_pin) { return false; }
+void change_pin_vulnerable(int user_i, unsigned short u_pin[], int new_pin) {
+    u_pin[user_i] = new_pin;  /* no validation, vulnerable */
+}
